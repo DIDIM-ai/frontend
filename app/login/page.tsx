0a9431a01b8f8aa25a9b9538b899978530d325f1
Hyein <snhyein2001@gmail.com>
2025-07-08 11:45:19 +0900

    feat: 로그인 페이지 UI

---
+++ app/login/page.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+export default function LoginPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-base mb-10">지금까지 분석 된 문제 수</p>
+      <h1 className="text-6xl font-bold text-primary drop-shadow-[0_8px_4px_rgba(255,119,16,0.6)] mb-20">
+        12,254
+      </h1>
+
+      <Image
+        src="/assets/login-illustration.svg"
+        alt="로그인 페이지 일러스트"
+        width={280}
+        height={197}
+        className="mb-16"
+        priority
+      />
+
+      <button className='cursor-pointer'>
+        <Image
+          src="/assets/kakao-login.png"
+          alt="카카오 로그인 버튼"
+          width={183}
+          height={45}
+          priority
+        />
+      </button>
+    </div>
+  );
+}
