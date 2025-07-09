66257eaab05a01cfe871b58d632c6d57722c73dc
hoon95 <maruon95@gmail.com>
2025-07-09 11:49:47 +0900

    feat: 분석결과 header 및 렌더링 조건 추가

---
+++ components/common/ResultHeader.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ChevronLeft } from 'lucide-react';
+
+export function ResultHeader() {
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
+
+  return (
+    <header className="relative mb-[35px] flex justify-center items-center h-10">
+      <button onClick={handleBack} className="absolute left-0 !cursor-pointer">
+        <ChevronLeft />
+      </button>
+
+      <h3 className="text-xl font-bold">분석 결과</h3>
+    </header>
+  );
+}
