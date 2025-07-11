f8e3258a6a63ed00b5b1d2c9b42667768f7cb22b
Hyein <snhyein2001@gmail.com>
2025-07-11 12:26:25 +0900

    style: 카카오 로그인 png -> svg

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,19 +19,24 @@ export default function LoginPage() {
         alt="로그인 페이지 일러스트"
         width={280}
         height={197}
-        className="mb-16"
+        className="mb-18"
         priority
       />
 
-      <button className="cursor-pointer">
-        <Image
-          src="/assets/kakao-login.png"
-          alt="카카오 로그인 버튼"
-          width={183}
-          height={45}
-          priority
-        />
-      </button>
+    <button
+      className="w-[200px] h-[45px] flex items-center justify-center gap-2 bg-[#FEE500] rounded-md shadow text-black font-semibold cursor-pointer"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-5 h-5"
+      >
+        <path d="M12 2C6.48 2 2 6.02 2 10.86c0 2.38 1.55 4.49 3.94 5.74-.11.79-.61 3.46-.71 4.15 0 0-.02.18.09.25.11.07.27-.01.27-.01 1.1-.15 4.48-2.91 5.15-3.47.4.05.81.08 1.26.08 5.52 0 10-4.02 10-8.86S17.52 2 12 2z" />
+      </svg>
+      <span>카카오 로그인</span>
+    </button>
+
     </div>
   );
 }
