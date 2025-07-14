ef43d822f3cb055ef7970bb211eb93c90afdf898
hoon95 <maruon95@gmail.com>
2025-07-14 10:20:48 +0900

    fix: RootHeader 아이콘 제거

--- components/common/RootHeader.tsx
+++ components/common/RootHeader.tsx
@@ -10,11 +10,6 @@ export function RootHeader() {
             <Image src="/assets/logo.png" alt="logo" width={80} height={50} />
           </Link>
         </li>
-        <li>
-          <Link href="/">
-            <Image src="/assets/profile.png" alt="logo" width={50} height={50} />
-          </Link>
-        </li>
       </nav>
     </header>
   );
