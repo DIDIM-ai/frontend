0bd2d4d615b38f874639c4c540c30363a5548520
hoon95 <maruon95@gmail.com>
2025-07-17 11:39:21 +0900

    feat: 결과페이지 - 문제 설명 및 단계 별 풀이 구성

--- app/result/[logSolvedId]/page.tsx
+++ app/result/[logSolvedId]/page.tsx
@@ -1,12 +1,57 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
 import { Problem } from '../components/Problem';
 import { Solve } from '../components/Solve';
 import { TextInput } from '../components/TextInput';
 
 export default function ResultPage() {
+  interface ProblemData {
+    problem: object;
+  }
+
+  const params = useParams();
+  const logSolvedId = params?.logSolvedId;
+
+  const [problem, setProblem] = useState<ProblemData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!logSolvedId) return;
+    const fetchProblem = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+        const res = await fetch(`${API_BASE_URL}/api/math/${logSolvedId}`);
+        if (!res.ok) throw new Error('문제 데이터를 불러오지 못했습니다.');
+        const data: ProblemData = await res.json();
+        setProblem(data);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('알 수 없는 오류');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProblem();
+  }, [logSolvedId]);
+
+  if (loading) return <div>문제 불러오는 중...</div>;
+  if (error) return <div>오류: {error}</div>;
+
+  console.log(problem);
+
   return (
     <>
-      <Problem />
-      <Solve />
+      <Problem problem={problem} />
+      <Solve problem={problem} />
       <TextInput />
     </>
   );
--- app/result/components/Solve.tsx
+++ app/result/components/Solve.tsx
@@ -1,9 +1,34 @@
-export function Solve() {
+interface ExplanationStep {
+  title: string;
+  description: string;
+}
+
+interface SolveProps {
+  problem: {
+    parent_explanation: string;
+    explanation_steps: ExplanationStep[];
+  };
+}
+
+export function Solve({ problem }: SolveProps) {
   return (
     <section className="mb-10">
-      <h3 className="text-xl font-bold mb-5">풀이</h3>
-      <p>풀이에 대한 내용입니다.</p>
-      <p>정답에 대한 내용입니다.</p>
+      <div className="mb-10">
+        <h3 className="text-xl text-primary font-bold mb-5">이렇게 알려주세요!</h3>
+        <p className="mb-5 text-gray-500">{problem.parent_explanation}</p>
+      </div>
+
+      <div className="">
+        <h3 className="text-xl font-bold">단계 별 풀이</h3>
+        <div className="flex flex-col gap-5">
+          {problem.explanation_steps.map((item, idx) => (
+            <div className="border-b border-gray-100 py-5" key={idx}>
+              <h3 className="text-lg font-semibold">{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
