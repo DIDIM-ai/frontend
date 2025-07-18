f06bee6ce508e4cc9881c49336eacb5660ceebbe
Hyein <snhyein2001@gmail.com>
2025-07-18 11:50:44 +0900

    style: success 페이지 로그인중입니다.. 삭제

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -12,15 +12,9 @@ export default function SuccessPage() {
 
     if (token) {
       localStorage.setItem('accessToken', token);
-      console.log('Access Token:', token);
-      router.replace('/users');
+      router.replace('/');
     }
   }, [router]);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">로그인 처리 중...</h1>
-      <p className="text-gray-500 mt-2">잠시만 기다려 주세요</p>
-    </div>
-  );
+  return null; 
 }
