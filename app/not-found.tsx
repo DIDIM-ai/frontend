a33e7ae9e4a4001f7150157dee2ad4660dd142e2
hoon95 <maruon95@gmail.com>
2025-07-25 17:02:29 +0900

    style: 예외 컴포넌트 높이 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-151px)] text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <div className="mb-4">
         <h3 className="text-6xl font-bold text-gray-800">404</h3>
         <p className="text-2xl text-gray-600 mt-4">페이지를 찾을 수 없습니다.</p>
