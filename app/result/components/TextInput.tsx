a4b46f1f11a94f22080757c2f83f4ab556ea6a31
hoon95 <maruon95@gmail.com>
2025-07-25 20:44:45 +0900

    style: 멤버십 가입 안내문구로 변경

--- app/result/components/TextInput.tsx
+++ app/result/components/TextInput.tsx
@@ -6,10 +6,11 @@ export function TextInput() {
       <div className="relative">
         <input
           type="text"
-          placeholder="궁금한 내용을 물어보세요!"
-          className="w-full h-[50px] border border-gray-300 rounded-sm px-4"
+          placeholder="멤버십 서비스를 준비 중입니다."
+          disabled
+          className="bg-gray-100 w-full h-[50px] border border-gray-300 rounded-sm px-4"
         />
-        <Send className="absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer" />
+        <Send className="absolute text-gray-500 top-1/2 right-4 -translate-y-1/2" />
       </div>
     </div>
   );
